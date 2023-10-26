4f7229625cb5f5dedc0cb84beef7ec62448ed04b
Volker <volker.schmid@regify.com>
2023-10-26 09:48:15 +0200

    Updated documentation

--- src/test/vaccinator_direct_test.js
+++ src/test/vaccinator_direct_test.js
@@ -14,14 +14,13 @@
     const kMaxElapsedTime = 500 // milliseconds, warn if someting takes longer
         , kText = 'The quick brown fox jumps over the lazy dog' // test content
         , kHexRegex = /^[0-9a-fA-F]+$/ // RegEx to validate hex string
-        , appId = 'Rc-De_6nyCbb' // a valid appId for testing
-        , infoColor = 'font-weight: bold; color: #004488; background-color: #f0f4ff'; // color for lines
+        , appId = 'Rc-De_6nyCbb'; // a valid appId for testing
 
 /*	test functions
     ======================================================================= */
 
 async function v2() {
-    console.info('%cStarting vaccinator direct test v2.', infoColor);
+    _headline("Starting vaccinator direct test V2");
 
     /**@type {VData} */ // TODO: Umlaute & Sonderzeichen & Nur UTF-8
     const vData = {
@@ -57,14 +56,14 @@ async function v2() {
     await v.init();
 
     if (v._useCache) {
-        console.info("wiping any cached values");
+        console.info("wiping any previously cached values");
         v.wipeCache();
     }
 
     let tmp;
 
     // encrypt / decrypt
-    console.info("%c-------------- encrypt / decrypt --------------", infoColor);
+    _headline("encrypt / decrypt");
     let key = await v._getCryptoKey(); // AES-GCM test
     tmp = await _test(() => v._encrypt('data', key), r => /^aes-256-gcm:\w{32}:\w+$/.test(r), 'Result should match the regex!');
     await _test(() => v._decrypt(tmp, key), r => 'data' == r, 'Result does not match!');
@@ -94,14 +93,14 @@ async function v2() {
     // });
 
     // helper
-    console.info("%c-------------- helper --------------", infoColor);
+    _headline("helper");
     await _test(() => v._generateRandom(16), r => ((tmp = r) && r.byteLength == 16), 'Array should be the same length!');
     await _test(() => v._buf2hex(tmp), r => kHexRegex.test(tmp = r), 'Result should match the regex!');
     await _test(() => v._hex2buf(tmp), r => (r instanceof Uint8Array), 'Result should be a Uint8Array!');
     await _test(() => Vaccinator._string2buffer(kText), r => kText == Vaccinator._buffer2string(r), 'Result should match!');
 
     // search internal
-    console.info("%c-------------- search internal --------------", infoColor);
+    _headline("search internal");
     await _test(() => v._searchHash('Hallo'), r => !!r, '!');
     await _test(() => v._searchHash(kText), r => r == '4919423bdcc82dae251d434e90992d15c3320a95597222b616d11b17e80fa1656bcb544a3147e793012298', 'Result should match the string!');
     await _test(() => v._searchHash(kText.substring(1)), r => r !== '4919423bdcc82dae251d434e90992d15c3320a95597222b616d11b17e80fa1656bcb544a3147e793012298', 'Result should not match the string!');
@@ -110,22 +109,22 @@ async function v2() {
 
     // store / cache
     if (v._useCache) {
-        console.info("%c-------------- store / cache --------------", infoColor);
+        _headline("store / cache");
         await _test(() => v._storeCache('1', vData), r => !r, 'Storing should not fail!');
         await _test(() => v._saveAppId(appId), r => !r, 'Saving AppId should not fail!');
         await _test(() => v._retrieveCache('1'), r => !!r, 'Result should be some data!');
         await _test(() => v._retrieveCache('_1'), r => !!r, 'Result should be null!', true);
         await _test(() => v._removeCache(['1', '2']), r => !r, 'Result should not fail!');
     } else {
-        console.info("%c-------------- skip store / cache test (useCache=false)--------------", infoColor);
+        _headline("skip store / cache test (useCache=false)");
     }
 
     // public functions
-    console.info("%c-------------- new --------------", infoColor);
+    _headline("new");
     await _test(() => v.new(vData), r => { if (!!r) { _pushVid(r);} return !!r; }, 'Result should be VID!'); // !!!! needed for further tests
 
     // get
-    console.info("%c-------------- get --------------", infoColor);
+    _headline("get");
     await _test(() => v.getServerInfo(), r => !!r, 'Result should not be null!');
     await _test(() => v.get(_lastVid), r => _validateMap(r), 'Result should match!');
     await _test(() => v.get(_lastVid, true), r => _validateMap(r), 'Result should match!');
@@ -137,14 +136,14 @@ async function v2() {
     await _test(() => v.get([_lastVid, nonSense]), r => r, 'Result should fail!', true);
 
     // update
-    console.info("%c-------------- update --------------", infoColor);
+    _headline("update");
     vData.data =    '{"firstname":"Dr. Patrick","lastname":"Star",'+
                     '"address_street":"Bikini-Street"}';
     await _test(() => v.update(_lastVid, vData), r => !r, 'Update should not fail!');
     await _test(() => v.update('_wrongVid', vData), r => !r, 'Update should have failed!', true);
 
     // search
-    console.info("%c-------------- search --------------", infoColor);
+    _headline("search");
     await _test(() => v.search('pat'), r => r.length == 1, 'Wrong number of results! (Clear DV vault DB?)');
     await _test(() => v.search(nonSense), r => r.length == 0, 'Wrong number of results!');
     await _test(() => v.search('patr sta'), r => r.length == 1, 'Wrong number of results! (Clear DV vault DB?)');
@@ -154,17 +153,17 @@ async function v2() {
 
     // wipe
     if (v._useCache) {
-        console.info("%c-------------- wipe --------------", infoColor);
+        _headline("wipe");
         await _test(() => v.wipe(_lastVid), r => r, 'Wiping should not return error!');
         await _test(() => v._retrieveCache(_lastVid), r => r === null, 'Result should be null!');
         await _test(() => v.get(_lastVid), r => _validateMap(r), 'Result should match!'); // will trigger re-download into cache
         await _test(() => v._retrieveCache(_lastVid), r => !!r, 'Result should not be null!');
     } else {
-        console.info("%c-------------- skip wipe testing (useCache=false) --------------", infoColor);
+        _headline("skip wipe testing (useCache=false)");
     }
 
     // publish
-    console.info("%c-------------- publish --------------", infoColor);
+    _headline("publish");
     await _test(() => v.publish(vData, 'password', 5), r => _pushVid(r), 'Publish should return VID!');
     await _test(() => v.getPublished(_lastVid, 'password'), r => _validateMap(r), 'getPublished should not be empty!');
     console.log("%cNext call will produce a crypto error in this log, which is expected!", "color: #006600; font-weight: bold;");
@@ -172,14 +171,14 @@ async function v2() {
     await _test(() => v.get(_lastVid), r => _validateMap(r), 'get with publish vid should fail!', true);
 
     // app-Id
-    console.info("%c-------------- appId --------------", infoColor);
+    _headline("appId");
     await _test(() => v.getAppId(), r => appId == r, 'Result should match!');
     await _test(() => v.getAppId(true), r => appId == r, 'Result should match!');
     await _test(() => Vaccinator.validateAppId(appId), r => !!r, 'Result should not be false!');
     await _test(() => Vaccinator.validateAppId(nonSense), r => !r, 'Result should not be true!');
 
     // delete
-    console.info("%c-------------- delete --------------", infoColor);
+    _headline("delete");
     await _test(() => v.delete(nonSense), r => !r, 'Deleting nonsense should fail!', true);
     await _test(() => v.delete(notExistingVid), r => !r, 'Deleting non existing VID should not fail!');
 
@@ -194,7 +193,7 @@ async function v2() {
     await _wait(kMaxElapsedTime); // wait for uncompleted functions
 
     // clean all created vid
-    console.info("%c-------------- cleanup --------------", infoColor);
+    _headline("cleanup");
     if(_vids.length) {
         await _test(() => v.delete(_vids), r => !r, 'Result should not fail!');
     }
@@ -203,12 +202,12 @@ async function v2() {
     if(_processes.length > 0) {
         console.warn(`${_totalProcessCount} tests excecuted but ${_processes.length} not completed: ${JSON.stringify(_processes)}`);
     } else {
-        console.info("%cAll tests excecuted.", infoColor);
+        _headline("FINISHED. All tests excecuted.");
     }
 }
 
 async function v1() {
-    console.info('Starting vaccinator direct test v1.');
+    _headline("Starting vaccinator direct test v1.");
 
     let vData = '{"firstname":"Spongebob","lastname":"Square Pants", '+
             '"gender":"male","address_street":"Bikinistreet", '+
@@ -286,7 +285,19 @@ function _log(e) {
 }
 
 /**
- *
+ * Outputs a fancy styled line in the console
+ * 
+ * @param {string} text Message to print
+ */
+function _headline(text) {
+    const infoColor = 'font-weight: bold; color: #004488; background-color: #f0f4ff';
+    console.info("%c  -------------- "+text+" --------------  ", infoColor);
+}
+
+/**
+ * Wait a given number of milliseconds. 
+ * Use with await to get the effect.
+ * 
  * @param {number} ms
  * @returns {Promise<void>}
  */
@@ -297,8 +308,14 @@ async function _wait(ms) {
 }
 
 /**
- *
- * @param {Map<string, string>} result
+ * Examines a Map with given vData structure.
+ * If all .status entries of all items matches 'expect' value,
+ * it returns true.
+ * 
+ * If at least one map entry.status does not match 'expect',
+ * it returns false.
+ * 
+ * @param {Map<string, string>} result Map of results in vData struct
  * @param {'OK'|'NOTFOUND'|'INVALID'} expect Default: `OK`
  * @returns {boolean}
  */
@@ -312,6 +329,8 @@ function _validateMap(result, expect = 'OK') {
 
 /**
  * Collects the pushed vid for later deletion.
+ * It set the global variable _lastVid and pushes
+ * the vid into the global _vids array.
  *
  * @param {string} vid
  * @return {String} same vid.
